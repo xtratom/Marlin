c5161ce422337280a14eb7b276092b15cae83b33
Victor Mateus Oliveira <rhapsodyv@gmail.com>
2020-11-10 20:07:32 -0300

    support for spi flash chip W25Qxx

--- Marlin/src/HAL/NATIVE_SIM/sim/application.h
+++ Marlin/src/HAL/NATIVE_SIM/sim/application.h
@@ -6,18 +6,6 @@
 #include "user_interface.h"
 
 #include "hardware/Heater.h"
-#if ANY(TFT_COLOR_UI, TFT_CLASSIC_UI, TFT_LVGL_UI)
-  #include "hardware/ST7796Device.h"
-  using DisplayDevice = ST7796Device;
-  #define DISPLAY_PARAM SCK_PIN, MISO_PIN, MOSI_PIN, TFT_CS_PIN, TOUCH_CS_PIN, TFT_DC_PIN, BEEPER_PIN, BTN_EN1, BTN_EN2, BTN_ENC, KILL_PIN
-#else
-  #include "hardware/ST7920Device.h"
-  using DisplayDevice = ST7920Device;
-  #define DISPLAY_PARAM LCD_PINS_D4, LCD_PINS_ENABLE, LCD_PINS_RS, BEEPER_PIN, BTN_EN1, BTN_EN2, BTN_ENC, KILL_PIN
-#endif
-#ifdef SDSUPPORT
-  #include "hardware/SDCard.h"
-#endif
 #include "hardware/print_bed.h"
 
 #include "visualisation.h"
@@ -49,12 +37,30 @@ struct GraphWindow : public UiWindow {
 };
 
 #include "src/inc/MarlinConfig.h"
+#if ANY(TFT_COLOR_UI, TFT_CLASSIC_UI, TFT_LVGL_UI)
+  #include "hardware/ST7796Device.h"
+  using DisplayDevice = ST7796Device;
+  #define DISPLAY_PARAM SCK_PIN, MISO_PIN, MOSI_PIN, TFT_CS_PIN, TOUCH_CS_PIN, TFT_DC_PIN, BEEPER_PIN, BTN_EN1, BTN_EN2, BTN_ENC, KILL_PIN
+#else
+  #include "hardware/ST7920Device.h"
+  using DisplayDevice = ST7920Device;
+  #define DISPLAY_PARAM LCD_PINS_D4, LCD_PINS_ENABLE, LCD_PINS_RS, BEEPER_PIN, BTN_EN1, BTN_EN2, BTN_ENC, KILL_PIN
+#endif
+#ifdef SDSUPPORT
+  #include "hardware/SDCard.h"
+#endif
+#if HAS_SPI_FLASH
+  #include "hardware/W25QxxDevice.h"
+#endif
 
 class Simulation {
 public:
 
   Simulation() :  hotend(HEATER_0_PIN, TEMP_0_PIN),
                   bed_heater(HEATER_BED_PIN, TEMP_BED_PIN)
+                  #if HAS_SPI_FLASH
+                  , spi_flash(SCK_PIN, MISO_PIN, MOSI_PIN, W25QXX_CS_PIN, SPI_FLASH_SIZE)
+                  #endif
                   #ifdef SDSUPPORT
                   , sd(SCK_PIN, MISO_PIN, MOSI_PIN, SDSS)
                   #endif
@@ -74,6 +80,9 @@ public:
 
   Heater hotend;
   Heater bed_heater;
+  #if HAS_SPI_FLASH
+    W25QxxDevice spi_flash;
+  #endif
   #ifdef SDSUPPORT
     SDCard sd;
   #endif
